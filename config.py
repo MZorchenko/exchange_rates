52c113b70d1f936b7623c70b6d39021ddd327ae3
MZorchenko <110279884+MZorchenko@users.noreply.github.com>
2022-10-22 23:39:53 +0300

    token

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TOKEN = "5671242442:AAGZHweAAe-7EMgXIO6k7X5MeqjK6e5BSrY"
+TOKEN = ""
 exchanges = {
     'доллар': 'USD',
     'евро': 'EUR',
     'рубль': 'RUB'
 }
 
-TOKEN_API = "w2pyNmK7s0qwEWAg6r5TsWZr7NxIoGJe"
+TOKEN_API = ""
